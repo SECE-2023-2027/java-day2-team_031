eb2f978167e21a3501ada067c1a97af7da10e854
NISA SOWBIKA <nisasowbika.ks2023cse@sece.ac.in>
2026-06-16 19:44:18 +0530

    Seconds_to_hh:mm:ss

---
+++ Prob_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Prob_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the seconds : ");
+        int time = sc.nextInt();
+        int hours = time / 3600;
+        int minutes = (time % 3600) / 60;
+        int seconds = time % 60;
+        System.out.println(hours + ":" + minutes + ":" + seconds);
+    }
+}
