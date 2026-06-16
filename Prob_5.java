8a6cdb2b9c2cabb3a2e7cf4ec6936ca9dae5a80a
NISA SOWBIKA <nisasowbika.ks2023cse@sece.ac.in>
2026-06-16 19:35:58 +0530

    Anagram

--- Prob_5.java
+++ Prob_5.java
@@ -3,20 +3,14 @@ import java.util.Scanner;
 public class Prob_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Input size
         System.out.print("Enter size of array: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
-
-        // Input elements
         System.out.println("Enter elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
-        // Input n (position from end)
         System.out.print("Enter n (from end): ");
         int k = sc.nextInt();
 
---
+++ Prob_7.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Prob_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("String-1: ");
+        String s1 = sc.next();
+
+        System.out.print("String-2: ");
+        String s2 = sc.next();
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        if (s1.length() != s2.length()) {
+            System.out.println("Check if two given strings are anagrams or not?: false");
+            return;
+        }
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        boolean result = Arrays.equals(arr1, arr2);
+
+        System.out.println("Check if two given strings are anagrams or not?: " + result);
+    }
+}
